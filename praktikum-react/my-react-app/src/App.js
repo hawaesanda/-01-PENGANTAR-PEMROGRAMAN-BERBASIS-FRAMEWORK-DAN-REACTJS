3f53996e648519e30f12b029bb45c83c80225816
awaaa <hawa.esanda@gmail.com>
2025-02-18 09:25:46 +0700

    feat:: Pertemuan2-ReactJs_Membuat-komponen-React

---
+++ praktikum-react/my-react-app/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+// Komponen header
+function Header() {
+  return(
+    <header>
+      <h1>Aplikasi React Saya</h1>
+    </header>
+  );
+}
+
+// Komponen Main
+function Main() {
+  return(
+    <main>
+      <h2>Selamat datang di Aplikasi React Saya!</h2>
+      <p>Ini adalah area konten utama.</p>
+    </main>
+  );
+}
+
+// Komponen footer
+function Footer() {
+  return(
+    <footer>
+      <p>&copy; 2025 Aplikasi React Saya</p>
+    </footer>
+  );
+}
+
+// Komponen App 
+function App() {
+  return(
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
